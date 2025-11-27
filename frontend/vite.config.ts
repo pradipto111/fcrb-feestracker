9f61b6fde3a135d98a101f9e4288f22547dcfb25
Pradipto Mondal <pradiptom@Pradiptos-MacBook-Pro-Work.local>
2025-11-28 02:33:23 +0530

    Bug fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    host: true
+  },
+  preview: {
+    port: 4173,
+    host: true,
+    strictPort: false
   }
 });
 
